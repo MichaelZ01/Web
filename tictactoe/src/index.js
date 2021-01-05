357d8ee3659b7d998ab1cd22711294a5840c7dcf
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-01-05 20:14:07 +1300

    Added draw message

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -134,6 +134,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (history.length > 9) {
+      status = 'Draw';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
